2d34ba59e5f819a280c09d012430351a26822ab1
AdamGibbs <adam-gibbs@outlook.com>
2022-09-25 20:52:26 +0100

    Download functionality to home

--- src/components/Download.vue
+++ src/components/Download.vue
@@ -1,4 +1,37 @@
-<script setup></script>
+<script setup>
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+router.push({ name: "home" });
+// if path ends in slash, remove it
+const removeSlash = (path) => {
+  if (path.endsWith("/")) {
+    return path.slice(0, -1);
+  }
+  return path.trim();
+};
+// method removes all text up to last slash
+const removePath = (path) => {
+  path = removeSlash(path);
+  const splitPath = path.split("/");
+  return splitPath[splitPath.length - 1];
+};
+const checkExists = (id) => {
+  axios.get(`http://tometoyouapi/details/${id}`).then((res) => {
+    return true;
+  });
+  return false;
+};
+const goToDownload = () => {
+  let path = document.getElementById("pathInput").value;
+  path = removeSlash(path);
+  const id = removePath(path);
+  if (checkExists(id)) {
+    router.push({ name: "download", params: { path } });
+  }
+};
+</script>
 
 <template>
   <section class="relative py-28 bg-indigo-600 overflow-hidden" id="download">
@@ -22,9 +55,10 @@
             <div class="w-full xl:flex-1">
               <input
                 class="p-3 xl:p-0 xl:pr-6 w-full font-medium text-gray-500 placeholder-gray-500 outline-none"
-                id="footerInput1-1"
+                id="pathInput"
                 type="text"
                 placeholder="Paste link or ID here"
+                v-on:keyup.enter="goToDownload"
               />
             </div>
             <div class="w-full xl:w-auto">
@@ -32,6 +66,7 @@
                 <button
                   class="py-4 px-8 w-full text-white font-semibold border border-indigo-700 focus:ring focus:ring-indigo-300 bg-indigo-600 hover:bg-indigo-700 transition ease-in-out duration-200"
                   type="button"
+                  @click="goToDownload"
                 >
                   Download
                 </button>
